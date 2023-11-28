a8640e58c801e5ab49c8feb0f165b2a701e7048e
jmillanacosta <javier.millan.acosta@gmail.com>
2023-11-28 19:16:56 +0100

    Dropped columns

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function fetchAndPrint() {
 
     get_publications(orcid, publications => {
         const resultsTable = document.getElementById('resultsTable');
-        resultsTable.innerHTML = '<tr><th>Title</th><th>DOI</th><th>PMID</th><th>PMCID</th><th>Source</th><th>License Type</th><th>OA Status</th></tr>'; // Clear previous results
+        resultsTable.innerHTML = '<tr><th>Title</th><th>Identifier</th><th>License Type</th><th>OA Status</th></tr>'; // Clear previous results
 
         if (publications) {
             publications.forEach(publication => {
@@ -36,28 +36,27 @@ function printPublicationInfo(publication, email, resultsTable) {
 
     // Check if the source is a valid DOI
     const doiPattern = /^(https:\/\/doi\.org\/)?(10\.\d{4,}\/\S+)$/;
-    const doiMatches = source.match(doiPattern);
-    const doiColumn = doiMatches ? `<td><a href="https://doi.org/${doiMatches[0]}" target="_blank">${doiMatches[2]}</a></td>` : '<td></td>';
+    const doiMatches = 'https://doi.org/'+ source.match(doiPattern);
+    const identifierLink = doiMatches ? `<a href="https://doi.org/${doiMatches[0]}" target="_blank">${doiMatches[2]}</a>` : source;
 
     // Check if the source is a valid PMID
     const pmidPattern = /^\d+$/;
-    const pmidColumn = pmidPattern.test(source) ? `<td><a href="https://www.ncbi.nlm.nih.gov/pubmed/${source}" target="_blank">${source}</a></td>` : '<td></td>';
+    const pmidLink = pmidPattern.test(source) ? `<a href="https://www.ncbi.nlm.nih.gov/pubmed/${source}" target="_blank">${source}</a>` : source;
 
     // Check if the source is a valid PMCID
     const pmcidPattern = /^PMC(\d+)$/;
-    const pmcidMatches = source.match(pmcidPattern);
-    const pmcidColumn = pmcidMatches ? `<td><a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC/${pmcidMatches[1]}" target="_blank">${source}</a></td>` : '<td></td>';
+    const pmcidLink = pmcidPattern.test(source) ? `<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC/${source}" target="_blank">${source}</a>` : source;
 
-    // Display in the 'Source' column if it's neither a DOI, PMID, nor PMCID
-    const sourceColumn = (!doiMatches && !pmidPattern.test(source) && !pmcidMatches) ?
-        `<td><a href="${source}" target="_blank">${source}</a></td>` : '<td></td>';
+    // Display in the 'Identifier' column 
+    const identifierLinkOther = (!doiMatches && !pmidPattern.test(source) && !pmcidPattern.test(source)) ?
+        `<a href="${source}" target="_blank">${source}</a>` : source;
 
     get_source_info(source, email, sourceInfo => {
         const oaStatus = sourceInfo ? sourceInfo['oa_status'] || 'Not available' : 'Not available';
         const bestOALocation = sourceInfo ? sourceInfo['best_oa_location'] || {} : {};
         const licenseType = bestOALocation['license'] || 'Unknown';
 
-        const row = `<tr><td>${title}</td>${doiColumn}${pmidColumn}${pmcidColumn}${sourceColumn}<td>${licenseType}</td><td>${oaStatus}</td></tr>`;
+        const row = `<tr><td>${title}</td><td>${identifierLinkOther}</td><td>${licenseType}</td><td>${oaStatus}</td></tr>`;
         resultsTable.innerHTML += row;
     });
 }
