c082c4941d7e1f9fdd68cb9ef64d27dde7cf7ee0
jmillanacosta <javier.millan.acosta@gmail.com>
2023-11-29 17:27:02 +0100

    Added data and scripts

--- js/validation.js
+++
@@ -1,9 +0,0 @@
-// validation.js
-
-function is_valid_orcid(orcid) {
-    return /^\d{4}-\d{4}-\d{4}-\d{3}(\d|X)$/.test(orcid);
-}
-
-function is_valid_email(email) {
-    return /^\S+@\S+\.\S+$/.test(email);
-}
